94dfff75612f6767a5d84ea9347d7226a1d30108
Tony Fong <TFong1@users.noreply.github.com>
2021-01-21 11:11:21 -0800

    Begin implementation of solve_freecell()

--- freecell_solver.py
+++ freecell_solver.py
@@ -6,17 +6,22 @@
 ###
 '''
 
+import numpy
+
+
 
 order_of_magnitude = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"]
 red_suits = ["H", "D"]
 black_suits = ["C", "S"]
 
-Heart_deck = []
-Diamond_deck = []
-Club_deck = []
-Spade_deck = []
+Heart_foundation = []
+Diamond_foundation = []
+Club_foundation = []
+Spade_foundation = []
 
-decks = [
+free_cells = ["","","",""]
+
+tableau_piles = [
     ["JC", "KH", "8C", "3H", "7C", "9S", "9H"],
     ["TD", "AH", "3C", "4C", "6H", "KD", "TH"],
     ["3S", "6D", "JD", "6C", "7H", "2H", "AC"],
@@ -27,9 +32,9 @@ decks = [
     ["6S", "JS", "3D", "5S", "8H", "TS"]
 ]
 
-def move_card(from_deck, to_deck):
-    tmp = from_deck.pop()
-    to_deck.append(tmp)
+def move_card(from_pile, to_pile):
+    tmp = from_pile.pop()
+    to_pile.append(tmp)
 
 def get_facecard(card):
     return card[0]
@@ -46,11 +51,58 @@ def are_cards_different_colors(card1, card2):
     else:
         return True
 
-###  Check if difference between cards is one (and from_deck < to_deck) and not the same suit
+###  Check if difference between cards is one (and from_pile < to_pile) and not the same suit
 ###  also take into account empty decks (in decks or freecells)?
-def is_valid_move(from_deck, to_deck):
-    from_card = from_deck[-1]
-    to_card = to_deck[-1]
-    return
+def is_valid_move(from_pile, to_pile):
+    from_card = from_pile[-1]
+    to_card = to_pile[-1]
+    return False
 
 
+def is_victory_condition():
+    if Heart_foundation[-1] == "KH" and Diamond_foundation[-1] == "KD" and \
+       Club_foundation[-1] == "KC" and Spade_foundation[-1] == "KS" :
+        return True
+    else:
+        return False
+
+def solve_freecell():
+    if is_victory_condition():
+        return True
+
+    # Go through all the tableau piles for a valid move
+
+    # forwards
+
+
+    # backwards
+
+    # Go through all the free cells for a valid move
+    # Move cards from free cell to tableau pile:
+    for free_cell in free_cells:
+        if free_cell = "" :
+            for tableau_pile in tableau_piles:
+                if is_valid_move(free_cell, tableau_pile):
+                    # ****** SPECIAL CASE:  MOVE FREE CELLS **********
+                    #move_card(free_cell, tableau_pile)
+
+                    if solve_freecell():
+                        return True
+                    
+                    # If we've gotten to this point, there was no solution for this chain
+                    # undo the move
+                    #move_card(tableau_pile, free_cell)
+    
+    # Move cards from tableau pile to free cell:
+
+    return False
+
+
+def print_solution():
+    print()
+
+if __name__ == "__main__":
+    if solve_freecell():
+        print_solution()
+    else:
+        print("Freecell puzzle not solvable.")
